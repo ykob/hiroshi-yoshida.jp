b90fef54bd96763a2e08f4b45d7933a3f6a9ef3f
yoichi kobayashi <info@tplh.net>
2015-08-01 12:46:06 +0900

    [add] wpテーマファイルの追加。

---
+++ dst/wp/wp-content/themes/hiroshi-yoshida.jp/header.php
@@ -0,0 +1,24 @@
+        <div class="global-header">
+          <div class="global-header-inner">
+            <div class="website-title">
+              <a href="/">
+                Yoshida Hiroshi 吉田博
+              </a>
+            </div>
+            <ul class="global-navi">
+              <li>
+                <a href="">profile</a>
+              </li>
+              <li>
+                <a href="">gallery</a>
+              </li>
+              <li>
+                <a href="">IMAGO</a>
+              </li>
+              <li>
+                <a href="">contact</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      
