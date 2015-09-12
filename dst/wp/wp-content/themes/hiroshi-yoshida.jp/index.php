71c2597b16ff3d3dc14d12f23cf5f6934c7959cd
yoichi kobayashi <info@tplh.net>
2015-09-12 22:36:36 +0900

    [add] head内をmodule化。

--- dst/wp/wp-content/themes/hiroshi-yoshida.jp/index.php
+++ dst/wp/wp-content/themes/hiroshi-yoshida.jp/index.php
@@ -7,29 +7,9 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title><?php echo $title; ?></title>
-    <meta name="keywords" content="吉田博,yoshida hiroshi">
-    <meta name="description" content="<?php echo $description; ?>">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
-    
-    <meta property="og:title" content="<?php echo $title; ?>">
-    <meta property="og:type" content="website">
-    <meta property="og:description" content="<?php echo $description; ?>">
-    <meta property="og:url" content="<?php echo $url; ?>">
-    <meta property="og:image" content="<?php echo $path; ?>/img/main.jpg">
-    
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:creator" content="@yoshidahiroshi8">
-    <meta name="twitter:title" content="<?php echo $title; ?>">
-    <meta name="twitter:description" content="<?php echo $description; ?>">
-    <meta name="twitter:image" content="<?php echo $path; ?>/img/main.jpg">
-    
-    <link rel="index" href="/">
-    <link rel="shortcut icon" href="/img/favicon.ico">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Josefin+Slab:400,300">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=IM+Fell+Great+Primer+SC">
-    <link rel="stylesheet" href="<?php echo $path; ?>/style.css">
-    <link rel="alternate" href="/feed/jstarted.xml" type="application/rss+xml" title="">
+<?php
+  include 'head.php';
+?>
   </head>
   <body>
     <div class="page">
--- dst/wp/wp-content/themes/hiroshi-yoshida.jp/single.php
+++ dst/wp/wp-content/themes/hiroshi-yoshida.jp/single.php
@@ -7,29 +7,9 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title></title>
-    <meta name="keywords" content="吉田博,yoshida hiroshi">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
-    
-    <meta property="og:title" content="">
-    <meta property="og:type" content="">
-    <meta property="og:description" content="">
-    <meta property="og:url" content="">
-    <meta property="og:image" content="">
-    
-    <meta name="twitter:card" content="summary_large_image">
-    <meta name="twitter:creator" content="@yoshidahiroshi8">
-    <meta name="twitter:title" content="">
-    <meta name="twitter:description" content="">
-    <meta name="twitter:image" content="">
-    
-    <link rel="index" href="/">
-    <link rel="shortcut icon" href="/img/favicon.ico">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Josefin+Slab:400,300">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=IM+Fell+Great+Primer+SC">
-    <link rel="stylesheet" href="<?php echo $path; ?>/style.css">
-    <link rel="alternate" href="/feed/jstarted.xml" type="application/rss+xml" title="">
+<?php
+  include 'head.php';
+?>
   </head>
   <body>
     <div class="page">
